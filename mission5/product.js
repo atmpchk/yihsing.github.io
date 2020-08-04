8a597f2e32f0b94ce4e1fd8dfe49c22ca98879cd
yihsingchen <yihsing.chen@simbo.com.tw>
2020-08-05 05:06:07 +0800

    finally

--- mission5/product.js
+++ mission5/product.js
@@ -6,15 +6,16 @@ Vue.component('product', {
       <span class="badge badge-secondary float-right ml-2">{{ product.category }}</span>
       <h5 class="card-title">{{ product.title }}</h5>
       <p class="card-text">{{ product.content }}</p>
+      <p v-if="product.description" class="alert alert-warning small">{{ product.description }}</p>
       <p class="card-text text-right">{{ product.price | formatCurrency }}</p>
     </div>
     <div class="card-footer">
       <div class="d-flex justify-content-around row">
-        <button type="button" class="col-5 btn btn-outline-secondary btn-sm" :disabled="isLoading">
+        <button type="button" class="col-5 btn btn-outline-secondary btn-sm" @click="getProduct(product)" :disabled="isLoading || product.description">
           <i v-if="isLoading" class="spinner-grow spinner-grow-sm"></i>
           詳細資訊
         </button>
-        <button type="button" class="col-5 btn btn-outline-primary btn-sm" @click="putToCart(product)" :disabled="isLoading">
+        <button type="button" class="col-5 btn btn-outline-primary btn-sm" @click="putToCart(product)" :disabled="isLoading || inCart">
           <i v-if="isLoading" class="spinner-grow spinner-grow-sm"></i>
           加入購物車
         </button>
@@ -22,11 +23,12 @@ Vue.component('product', {
     </div>
   </div>
   `,
-  props: [ 'product' ],
+  props: [ 'product', 'inCart' ],
   data () {
     return {
       apiInfo: {
-        forCart: '/ec/shopping'
+        forCart: '/ec/shopping',
+        forProduct: '/ec/product'
       },
       isLoading: false
     }
@@ -40,6 +42,17 @@ Vue.component('product', {
         product: product.id,
         quantity: 1
       }).then((result) => {
+        this.$emit('put-to-cart', product.id)
+        this.isLoading = false
+      }).catch((err) => {
+        this.isLoading = false
+        console.log(err)
+      })
+    },
+    getProduct (product) {
+      this.isLoading = true
+      axios.get(`${this.apiInfo.forProduct}/${product.id}`).then((result) => {
+        product.description = result.data.data.description
         this.isLoading = false
       }).catch((err) => {
         this.isLoading = false
--- mission5/products.js
+++ mission5/products.js
@@ -4,20 +4,43 @@ new Vue({
     apiInfo: {
       UUID: '840d218c-ef17-4e49-90d3-cfef6170a5e5',
       token: '',
+      forCart: '/ec/shopping',
       forProducts: '/ec/products'
     },
+    productIdsInCart: [],
     products: [],
     isLoading: false,
     isFullPageLoading: true,
     pagination: {}
   },
+  computed: {
+    isInCart () {
+      return (product) => {
+        return this.productIdsInCart.includes(product.id)
+      }
+    }
+  },
   created () {
     axios.defaults.baseURL = `https://course-ec-api.hexschool.io/api/${this.apiInfo.UUID}/`
   },
   mounted () {
+    this.getProductIdsInCart()
     this.getProducts()
   },
   methods: {
+    getProductIdsInCart (page) {
+      if (!page) {
+        page = this.pagination.current_page || 1
+      }
+      this.isLoading = true
+      axios.get(`${this.apiInfo.forCart}?page=${page}`).then((result) => {
+        this.productIdsInCart = result.data.data.map(item => item.product.id)
+        this.isLoading = false
+      }).catch((err) => {
+        this.isLoading = false
+        console.log(err)
+      })
+    },
     getProducts (page) {
       if (!page) {
         page = this.pagination.current_page || 1
@@ -31,6 +54,9 @@ new Vue({
         this.isLoading = false
         console.log(err)
       })
+    },
+    putToCart (id) {
+      this.productIdsInCart.push(id)
     },
     viewCart () {
       window.location = 'cart.html'
