15db48d72fb11ae69e375c4c6eb6e1e1e5082d43
yihsingchen <yihsing.chen@simbo.com.tw>
2020-08-18 01:42:43 +0800

    event bus and admin token check

--- mission7/src/main.js
+++ mission7/src/main.js
@@ -1,14 +1,16 @@
 import Vue from 'vue';
+import { registerGlobally, setDefaultBaseUrlFromEnv } from './assets/js/vue/axios';
 import App from './App.vue';
 import router from './router';
 import formatter from './assets/js/formatter';
-import { registerGlobally, setDefaultBaseUrlFromEnv } from './assets/js/vue/axios';
 import 'bootstrap';
 import './assets/js/jquery';
 import './assets/js/vue/loading';
 import './assets/js/vue/veeValidate';
 
 Vue.config.productionTip = false;
+
+Vue.prototype.$bus = new Vue();
 
 registerGlobally();
 setDefaultBaseUrlFromEnv();
