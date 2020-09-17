41b2ebcd1dd6cbabd16ed6bc241ec8800967a2cf
yihsingchen <yihsing.chen@simbo.com.tw>
2020-09-17 08:02:05 +0800

    add image

--- mission8/src/views/attraction/Index.vue
+++ mission8/src/views/attraction/Index.vue
@@ -5,16 +5,15 @@
         <div class="col-md-4 mt-md-4">
           <div class="card border-0 mb-4">
             <img
-              src="https://images.unsplash.com/photo-1502743780242-f10d2ce370f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1916&q=80"
+              src="https://hexschool-api.s3.us-west-2.amazonaws.com/custom/cSYtrAzitF1ra1W4KKyRDbVIVZl8GGQ2kyEnBTSaT12nHZsyttFxbTlezrBtgMUR8teYoygLX79HQeY3FU2tYBAw3bRv0jBdrtFcTMw06wdCHV0HKAOjdkxsO5OOE55x.jpg"
               class="card-img-top rounded-0"
               alt="..."
             />
             <div class="card-body text-center">
-              <h4>Lorem ipsum</h4>
+              <h4>純粹巧克力</h4>
               <div class="d-flex justify-content-between">
                 <p class="card-text text-muted mb-0">
-                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                  diam nonumy eirmod.
+                  百分百的純粹、體驗那箇中的滋味、適合忠於原味的您
                 </p>
               </div>
             </div>
@@ -23,16 +22,15 @@
         <div class="col-md-4 mt-md-4">
           <div class="card border-0 mb-4">
             <img
-              src="https://images.unsplash.com/photo-1502743780242-f10d2ce370f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1916&q=80"
+              src="https://hexschool-api.s3.us-west-2.amazonaws.com/custom/JAMttJut08MOk3yaLgBb7KS2o9yp7dMQcvpqKr7SCC1vx6vsLKX9T7KpaFUbwNcxquoqnHAQxnvUXr9fY8u5c3s7WicHyi6UwjsIChfQKeoqL1JC8H2FbcXfl9ypChjw.jpg"
               class="card-img-top rounded-0"
               alt="..."
             />
             <div class="card-body text-center">
-              <h4>Lorem ipsum</h4>
+              <h4>牛奶巧克力</h4>
               <div class="d-flex justify-content-between">
                 <p class="card-text text-muted mb-0">
-                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                  diam nonumy eirmod.
+                  當純粹巧克力碰上香醇牛奶、美妙的融合一起、教人如何拒絕
                 </p>
               </div>
             </div>
@@ -41,16 +39,15 @@
         <div class="col-md-4 mt-md-4">
           <div class="card border-0 mb-4">
             <img
-              src="https://images.unsplash.com/photo-1502743780242-f10d2ce370f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1916&q=80"
+              src="https://hexschool-api.s3.us-west-2.amazonaws.com/custom/8QpJ6wLdL4MqQZumpTKyQ0u6e5L8H2kN4zLV9qJ0ItU55HQfy7QFVrG2fsRnFGn4dPbXATaJONcN84v0CO6uNCCaOiCsp8DBEhpjLA5e8Fott7spnu0l8FJuPkV7PKSQ.jpg"
               class="card-img-top rounded-0"
               alt="..."
             />
             <div class="card-body text-center">
-              <h4>Lorem ipsum</h4>
+              <h4>典藏巧克力</h4>
               <div class="d-flex justify-content-between">
                 <p class="card-text text-muted mb-0">
-                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                  diam nonumy eirmod.
+                  榛果、巧心、酥脆表皮、灑上可可粉、淋上特製可可醬...
                 </p>
               </div>
             </div>
@@ -76,5 +73,5 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 </style>
